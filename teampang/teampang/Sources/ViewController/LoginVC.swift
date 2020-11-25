110df026202a3b2d59530e205348952d202afc41
MinseungSeon <seonminseung@naver.com>
2020-11-26 01:47:36 +0900

    add: 키보드에 따른 화면 이동, 화면 터치하면 키보드 자동으로 내려가는 기능 추가

--- teampang/teampang/Sources/ViewController/LoginVC.swift
+++ teampang/teampang/Sources/ViewController/LoginVC.swift
@@ -7,19 +7,22 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
-
-    @IBOutlet var logo: UIImageView!
-    @IBOutlet var loginTextField: UITextField!
-    @IBOutlet var loginButtonWidth: NSLayoutConstraint!
+class LoginVC: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var pwTextField: PwTextField!
+    @IBOutlet var loginButtonWidth: NSLayoutConstraint!
+    @IBOutlet var loginTextFieldWidth: NSLayoutConstraint!
+    @IBOutlet var pwTextFieldWidth: NSLayoutConstraint!
+    @IBOutlet var spacing01: NSLayoutConstraint!
+    @IBOutlet var spacing02: NSLayoutConstraint!
     @IBOutlet var findId: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        autoLayoutFunc()
+        keyboardFunc()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -35,14 +38,39 @@ class LoginVC: UIViewController {
         self.navigationController?.pushViewController(signUpVC, animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func autoLayoutFunc() {
+        let screenHeight = UIScreen.main.bounds.height
+        let autoLayoutWidth = 45/896*screenHeight
+        self.loginButtonWidth.constant = autoLayoutWidth
+        self.loginTextFieldWidth.constant = autoLayoutWidth
+        self.pwTextFieldWidth.constant = autoLayoutWidth
+        self.spacing01.constant = 20/896*screenHeight
+        self.spacing02.constant = 35/896*screenHeight
     }
-    */
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pwTextField.resignFirstResponder()
+        return true
+    }
+    
+    @objc
+    func keyboardWillShow(_ sender: Notification) {
+        self.view.frame.origin.y = -150
+    }
+    @objc
+    func keyboardWillHide(_ sender: Notification){
+        self.view.frame.origin.y = 0
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
+    
+    func keyboardFunc(){
+        pwTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 
 }
--- teampang/teampang/Sources/ViewController/SignUpVC.swift
+++ teampang/teampang/Sources/ViewController/SignUpVC.swift
@@ -7,19 +7,78 @@
 
 import UIKit
 
-class SignUpVC: UIViewController {
+class SignUpVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var fieldWidth01: NSLayoutConstraint!
+    @IBOutlet var fieldWidth02: NSLayoutConstraint!
+    @IBOutlet var fieldWidth03: NSLayoutConstraint!
+    @IBOutlet var fieldWidth04: NSLayoutConstraint!
+    @IBOutlet var signupButtonWidth: NSLayoutConstraint!
+    
+    @IBOutlet var spacing01: NSLayoutConstraint!
+    @IBOutlet var spacing02: NSLayoutConstraint!
+    @IBOutlet var spacing03: NSLayoutConstraint!
+    
+    @IBOutlet var spacing04: NSLayoutConstraint!
+    @IBOutlet var spacing05: NSLayoutConstraint!
+    
+    @IBOutlet var pwCheckTextField: PwCheckTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayout()
+        autoLayoutFunc()
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        keyboardFunc()
+    }
+    func setLayout() {
         self.navigationController?.navigationBar.topItem?.title = ""
         self.navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.barTintColor = UIColor.init(red: 41/255, green: 157/255, blue: 243/255, alpha: 1)
         self.title = "회원가입"
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-        // Do any additional setup after loading the view.
     }
+    func autoLayoutFunc() {
+        let screenHeight = UIScreen.main.bounds.height
+        let autoLayoutWidth = 45/896*screenHeight
+        self.fieldWidth01.constant = autoLayoutWidth
+        self.fieldWidth02.constant = autoLayoutWidth
+        self.fieldWidth03.constant = autoLayoutWidth
+        self.fieldWidth04.constant = autoLayoutWidth
+        self.signupButtonWidth.constant = autoLayoutWidth
+        self.spacing01.constant = 50/896*screenHeight
+        self.spacing02.constant = 25/896*screenHeight
+        self.spacing03.constant = 25/896*screenHeight
+        self.spacing04.constant = 25/896*screenHeight
+        self.spacing05.constant = 50/896*screenHeight
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pwCheckTextField.resignFirstResponder()
+        return true
+    }
+    
+    @objc
+    func keyboardWillShow(_ sender: Notification) {
+        self.view.frame.origin.y = -150
+    }
+    @objc
+    func keyboardWillHide(_ sender: Notification){
+        self.view.frame.origin.y = 0
 
-
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
+    
+    func keyboardFunc(){
+        pwCheckTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 
 }
